e1f89701743ec9d5de606397641d9151c17c34d5
ShreyaSanjayAkole <shreyaakole143@gmail.com>
2024-11-08 11:23:52 +0530

    Last name

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,8 +1,4 @@
-
-function validateFirstName(firstName) {
-  const regex = /^[A-Z][a-zA-Z]{2,}$/;
-  return regex.test(firstName);
-}
+import { validateFirstName, validateLastName } from './Validation.js';
 
 const firstName = prompt("Enter your first name:");
 if (validateFirstName(firstName)) {
@@ -11,4 +7,12 @@ if (validateFirstName(firstName)) {
   console.log("Invalid first name. It must start with a capital letter and be at least 3 characters long.");
 }
 
+const lastname = prompt("Enter your last name:");
+if (validateLastName(lastname)){
+  console.log("valid last name");
+}else{
+  console.log("Invalid last name. It must start with a capital letter and be at least 3 characters long.");
+}
+
 window.validateFirstName = validateFirstName;
+window.validateLastName = validateLastName;
