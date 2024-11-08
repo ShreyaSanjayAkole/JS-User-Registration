90963c971ee06b1e4f10ef65d1d54eaad97f8b99
ShreyaSanjayAkole <shreyaakole143@gmail.com>
2024-11-08 11:24:23 +0530

    Regex pattern for validation

--- Validation.js
+++ Validation.js
@@ -3,4 +3,9 @@ export function validateFirstName(name) {
     const regex = /^[A-Z][a-zA-Z]{2,}$/;
     return regex.test(name);
   }
+
+export function validateLastName(name){
+  const regex = /^[A-Z][a-zA-Z]{2,}$/;
+  return regex.test(name);
+}
   
